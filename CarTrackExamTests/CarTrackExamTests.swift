1a1a80e8d23b7b47fc27994a8ff3f9dabd3e4146
Ray Bryan Vivo <rbzvivo@multisyscorp.com>
2020-09-16 21:26:03 +0800

    added unit tests

--- CarTrackExamTests/CarTrackExamTests.swift
+++ CarTrackExamTests/CarTrackExamTests.swift
@@ -11,24 +11,46 @@ import XCTest
 
 class CarTrackExamTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testVerifyUser() {
+        let expection = expectation(description: "Expecting to be able to login")
+        let viewModel = LoginViewModel()
+        viewModel.login(name: "TestUser", password: "Test123!", country: "Philippines")
+        viewModel.verifySuccess = {
+            expection.fulfill()
         }
+        waitForExpectations(timeout: 5)
     }
-
+    
+    func testVerifyUserFailed() {
+        let expection = expectation(description: "Expecting to not be able to login")
+        let viewModel = LoginViewModel()
+        viewModel.login(name: "TestUser", password: "Test", country: "United States")
+        viewModel.verifyFailed = {
+            expection.fulfill()
+        }
+        waitForExpectations(timeout: 5)
+    }
+    
+    func testRetrieveUsers() {
+        let expection = expectation(description: "Expecting to get users from api")
+        let viewModel = UserListViewModel()
+        viewModel.retrieveUsers()
+        viewModel.fetchUsersCompleted = {
+            XCTAssertGreaterThan(viewModel.users.count, 0)
+            expection.fulfill()
+        }
+        waitForExpectations(timeout: 20)
+    }
+    
+    func testCountrySearch() {
+        let viewModel = CountryListViewModel()
+        viewModel.searchCountry(searchText: "Singapore")
+        XCTAssertEqual("Singapore", viewModel.searchList.first)
+    }
+    
+    func testCountrySearchFailed() {
+        let viewModel = CountryListViewModel()
+        viewModel.searchCountry(searchText: "1234")
+        XCTAssertTrue(viewModel.searchList.count == 0)
+    }
 }
